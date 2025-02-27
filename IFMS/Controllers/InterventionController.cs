bd45e4d0acb78d621c0f5f6797a1c350b3fd4b0f
Gergo Halasz <halasz.gergo2001@gmail.com>
2025-02-27 17:06:25 +0200

    Add project files.

---
+++ IFMS/Controllers/InterventionController.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Application.Features.Interventions.Commands;
+using Application.Features.Interventions.Queries;
+using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class InterventionController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public InterventionController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateIntervention([FromBody] CreateInterventionDto interventionDto)
+		{
+			var command = new CreateInterventionCommand(interventionDto);
+			var interventionId = await _mediator.Send(command);
+			return CreatedAtAction(nameof(GetInterventionById), new { id = interventionId }, interventionId);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllInterventions()
+		{
+			return Ok();  
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetInterventionById(int id)
+		{
+			var query = new GetInterventionByIdQuery(id);
+			var intervention = await _mediator.Send(query);
+			if (intervention == null) return NotFound();
+			return Ok(intervention);
+		}
+	}
+}
---
+++ IFMS/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Application.Features.Interventions.Handlers;
+using Infrastructure;
+using Infrastructure.Persistence.DbContexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetInterventionByIdQueryHandler>());
+
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "Intervention API",
+		Version = "v1",
+		Description = "API for managing interventions"
+	});
+});
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll",
+		policy => policy.AllowAnyOrigin()
+						.AllowAnyMethod()
+						.AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Intervention API v1");
+	c.RoutePrefix = string.Empty;
+});
+app.UseSwagger();
+
+if (app.Environment.IsDevelopment())
+{
+	app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
